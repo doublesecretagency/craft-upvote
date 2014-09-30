94816efeaed0d0608b024620ca6d2cf5ed0c8475
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2014-09-30 16:54:01 -0700

    Reverted orderElementsByScore logic

--- upvote/services/Upvote_QueryService.php
+++ upvote/services/Upvote_QueryService.php
@@ -25,12 +25,34 @@ class Upvote_QueryService extends BaseApplicationComponent
 	}
 
 
+	/*
 	// 
 	public function orderElementsByScore(ElementCriteriaModel $criteria) {
 		$query = craft()->elements->buildElementsQuery($criteria);
 		$query->join('upvote_elementscores upvote_elementscores', 'upvote_elementscores.id = elements.id');
 		$query->order('upvote_elementscores.score DESC, upvote_elementscores.id ASC');
 		return $query->queryAll();
+	}
+	*/
+
+	//
+	public function orderElementsByScore(ElementCriteriaModel $criteria) {
+		$elementIds = $this->_elementIdsByScore();
+		$criteria->setAttribute('id', $elementIds);
+		$criteria->setAttribute('order', 'FIELD(elements.id, '.join(', ', $elementIds).')');
+		return $criteria;
+	}
+
+	//
+	private function _elementIdsByScore() {
+		$scores = Upvote_ElementScoreRecord::model()->findAll(array(
+			'order' => 'score DESC, id ASC'
+		));
+		$elementIds = array();
+		foreach ($scores as $score) {
+			$elementIds[] = $score->id;
+		}
+		return $elementIds;
 	}
 
 }
